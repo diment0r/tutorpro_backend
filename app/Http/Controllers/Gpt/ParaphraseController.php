317458bb995d55c3ff29c14b254541775c14b42e
dimentor <dmitriyprudnik.workmail@gmail.com>
2024-05-13 18:53:46 +0300

    refactor

--- app/Http/Controllers/Gpt/ParaphraseController.php
+++ app/Http/Controllers/Gpt/ParaphraseController.php
@@ -13,6 +13,60 @@ use Illuminate\Support\Facades\Http;
 
 class ParaphraseController extends Controller
 {
+    private function gptRequest($isJsonResponseFormat, $requestContent)
+    {
+        $messages = [
+            ['role' => 'system', 'content' => $isJsonResponseFormat ? 'Provide a valid JSON output' : ''],
+            ['role' => 'user', 'content' => $requestContent],
+        ];
+        return Http::withHeaders([
+            'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
+            'Content-Type' => 'application/json',
+        ])->post(config('gpt.openai.api_url'), [
+            'response_format' => $isJsonResponseFormat ? ['type' => 'json_object'] : null,
+            'messages' => $messages,
+
+            'model' => config('gpt.openai.model'),
+            'temperature' => (float)config('gpt.openai.temperature'),
+            'max_tokens' => config('gpt.openai.max_tokens'),
+            'top_p' => (float)config('gpt.openai.top_p'),
+            'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
+            'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
+            'stop' => [config('gpt.openai.stop')],
+        ])->json();
+    }
+
+    private function storeParaphrase($subject, $topic, $paraphrase)
+    {
+        $createdParaphraseId = Paraphrase::create([
+            'subject' => $subject,
+            'topic' => $topic,
+            'paraphrase' => $paraphrase,
+            'user_id' => auth('sanctum')->user()->id,
+        ])->id;
+        return Paraphrase::with('user')->where('id', $createdParaphraseId)->first();
+    }
+
+    private function storeParaphraseTest($test, $paraphraseId)
+    {
+        foreach ($test->questions as $question) {
+            $questionId = Question::create([
+                'question' => $question->question,
+                'paraphrase_id' => $paraphraseId,
+            ])->id;
+
+            for ($i = 0; $i < 4; $i++) {
+                Option::create([
+                    'option' => $question->options[$i],
+                    'correct' => $i == $question->correct,
+                    'question_id' => $questionId,
+                ]);
+            }
+        }
+
+        return Paraphrase::where('id', $paraphraseId)->with('questions.options')->first()->questions;
+    }
+
     public function defaultTopicParaphrase(DefaultParaphraseRequest $request)
     {
         $request->validated();
@@ -27,7 +81,6 @@ class ParaphraseController extends Controller
         }
 
         $paraphrase = Paraphrase::with('user')->where('topic', $request->topic)->first();
-
         if ($paraphrase) {
             return response()->json([
                 'success' => true,
@@ -35,27 +88,8 @@ class ParaphraseController extends Controller
             ]);
         }
 
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
-            'Content-Type' => 'application/json',
-        ])->post(config('gpt.openai.api_url'), [
-            // 'response_format' => ['type' => 'json_object'],
-            'messages' => [
-                [
-                    'role' => 'user',
-                    'content' => 'Розкажи основну теоретичну інформацію про "' . $request->topic . '" з предмету ' . $request->subject,
-                ]
-            ],
-
-            'model' => config('gpt.openai.model'),
-            'temperature' => (float)config('gpt.openai.temperature'),
-            'max_tokens' => config('gpt.openai.max_tokens'),
-            'top_p' => (float)config('gpt.openai.top_p'),
-            'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
-            'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
-            'stop' => [config('gpt.openai.stop')],
-        ])->json();
-
+        $propmt = 'Розкажи основну теоретичну інформацію про "' . $request->topic . '" з предмету ' . $request->subject;
+        $response = $this->gptRequest(false, $propmt);
         if (!array_key_exists('choices', $response)) {
             return response()->json([
                 'success' => false,
@@ -65,15 +99,7 @@ class ParaphraseController extends Controller
             ]);
         }
 
-        $createdParaphraseId = Paraphrase::create([
-            'subject' => $request->subject,
-            'topic' => $request->topic,
-            'paraphrase' => $response['choices'][0]['message']['content'],
-            'user_id' => auth('sanctum')->user()->id,
-        ])->id;
-
-        $paraphrase = Paraphrase::with('user')->where('id', $createdParaphraseId)->first();
-
+        $paraphrase = $this->storeParaphrase($request->subject, $request->topic, $response['choices'][0]['message']['content']);
         return response()->json([
             'success' => true,
             'data' => $paraphrase,
@@ -83,7 +109,6 @@ class ParaphraseController extends Controller
     public function literatureTopicParaphrase(LiteratureParaphraseRequest $request)
     {
         $request->validated();
-
         if (!(strtolower($request->size) == 'short' || strtolower($request->size) == 'long')) {
             return response()->json([
                 'success' => false,
@@ -94,7 +119,6 @@ class ParaphraseController extends Controller
         }
 
         $paraphrase = Paraphrase::with('user')->where('topic', $request->topic . ' [' . $request->size . ']')->first();
-
         if ($paraphrase) {
             return response()->json([
                 'success' => true,
@@ -103,27 +127,8 @@ class ParaphraseController extends Controller
         }
 
         $paraphraseSizePrompt = strtolower($request->size) == 'short' ? config('gpt.paraphrase_size.short') : config('gpt.paraphrase_size.long');
-
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
-            'Content-Type' => 'application/json',
-        ])->post(config('gpt.openai.api_url'), [
-            'messages' => [
-                [
-                    'role' => 'user',
-                    'content' => 'Розкажи послідовно основні події літературного твору "' . $request->topic . '", дійові особи, тема, думка твору. ' . $paraphraseSizePrompt,
-                ]
-            ],
-
-            'model' => config('gpt.openai.model'),
-            'temperature' => (float)config('gpt.openai.temperature'),
-            'max_tokens' => config('gpt.openai.max_tokens'),
-            'top_p' => (float)config('gpt.openai.top_p'),
-            'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
-            'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
-            'stop' => [config('gpt.openai.stop')],
-        ])->json();
-
+        $propmt = 'Розкажи послідовно основні події літературного твору "' . $request->topic . '", дійові особи, тема, думка твору. ' . $paraphraseSizePrompt;
+        $response = $this->gptRequest(false, $propmt);
         if (!array_key_exists('choices', $response)) {
             return response()->json([
                 'success' => false,
@@ -133,15 +138,7 @@ class ParaphraseController extends Controller
             ]);
         }
 
-        $createdParaphraseId = Paraphrase::create([
-            'subject' => config('gpt.subjects.literature'),
-            'topic' => $request->topic . ' [' . $request->size . ']',
-            'paraphrase' => $response['choices'][0]['message']['content'],
-            'user_id' => auth('sanctum')->user()->id,
-        ])->id;
-
-        $paraphrase = Paraphrase::with('user')->where('id', $createdParaphraseId)->first();
-
+        $paraphrase = $this->storeParaphrase(config('gpt.subjects.literature'), $request->topic . ' [' . $request->size . ']', $response['choices'][0]['message']['content']);
         return response()->json([
             'success' => true,
             'data' => $paraphrase,
@@ -151,7 +148,6 @@ class ParaphraseController extends Controller
     public function paraphraseTest($paraphraseId)
     {
         $paraphrase = Paraphrase::where('id', $paraphraseId)->with('questions.options')->first();
-
         if (!$paraphrase) {
             return response()->json([
                 'success' => false,
@@ -168,31 +164,8 @@ class ParaphraseController extends Controller
             ]);
         }
 
-        $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
-            'Content-Type' => 'application/json',
-        ])->post(config('gpt.openai.api_url'), [
-            'response_format' => ['type' => 'json_object'],
-            'messages' => [
-                [
-                    'role' => 'system',
-                    'content' => 'Provide a valid JSON output'
-                ],
-                [
-                    'role' => 'user',
-                    'content' => 'Сфорулюй 5 тестових запитань до цього тексту ' . $paraphrase->paraphrase . ' , який є стислим переказом теми ' . $paraphrase->topic . ' з предмету ' . $paraphrase->subject . '. На кожне запитання 4 варіанти відповіді, де тільки одна правильна. Запитання мають бути виключно українською мовою. Всі запитання надай у полі "questions" як масив запитань. В об\'єкті питання має бути поле "question" значенням якого є текст запитання. Масив "options" з переліком варіантів відповіді. Та "correct" значення якого індекс правильного варіанту відповіді з масиву "options"',
-                ]
-            ],
-
-            'model' => config('gpt.openai.model'),
-            'temperature' => (float)config('gpt.openai.temperature'),
-            'max_tokens' => config('gpt.openai.max_tokens'),
-            'top_p' => (float)config('gpt.openai.top_p'),
-            'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
-            'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
-            'stop' => [config('gpt.openai.stop')],
-        ])->json();
-
+        $propmt = 'Сфорулюй 5 тестових запитань до цього тексту ' . $paraphrase->paraphrase . ' , який є стислим переказом теми ' . $paraphrase->topic . ' з предмету ' . $paraphrase->subject . '. На кожне запитання 4 варіанти відповіді, де тільки одна правильна. Запитання мають бути виключно українською мовою. Всі запитання надай у полі "questions" як масив запитань. В об\'єкті питання має бути поле "question" значенням якого є текст запитання. Масив "options" з переліком варіантів відповіді. Та "correct" значення якого індекс правильного варіанту відповіді з масиву "options"';
+        $response = $this->gptRequest(true, $propmt);
         if (!array_key_exists('choices', $response)) {
             return response()->json([
                 'success' => false,
@@ -202,26 +175,10 @@ class ParaphraseController extends Controller
             ]);
         }
 
-        $test = json_decode($response['choices'][0]['message']['content']);
-        foreach ($test->questions as $question) {
-            $questionId = Question::create([
-                'question' => $question->question,
-                'paraphrase_id' => $paraphraseId,
-            ])->id;
-
-            for ($i = 0; $i < 4; $i++) {
-                Option::create([
-                    'option' => $question->options[$i],
-                    'correct' => $i == $question->correct,
-                    'question_id' => $questionId,
-                ]);
-            }
-        }
-
-        $paraphrase = Paraphrase::where('id', $paraphraseId)->with('questions.options')->first();
+        $questions = $this->storeParaphraseTest(json_decode($response['choices'][0]['message']['content']), $paraphraseId);
         return response()->json([
             'success' => true,
-            'data' => $paraphrase->questions,
+            'data' => $questions,
         ]);
     }
 }
@@ -235,3 +192,103 @@ class ParaphraseController extends Controller
 
 // "topic": "Русь-Україна: культура IX–XIV століть",
 // "subject": "історія"
+
+
+// ? DEFAULT PARAPHRASE
+// $response = Http::withHeaders([
+        //     'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
+        //     'Content-Type' => 'application/json',
+        // ])->post(config('gpt.openai.api_url'), [
+        //     // 'response_format' => null,
+        //     'messages' => [
+        //         [
+        //             'role' => 'user',
+        //             'content' => 'Розкажи основну теоретичну інформацію про "' . $request->topic . '" з предмету ' . $request->subject,
+        //         ]
+        //     ],
+
+        //     'model' => config('gpt.openai.model'),
+        //     'temperature' => (float)config('gpt.openai.temperature'),
+        //     'max_tokens' => config('gpt.openai.max_tokens'),
+        //     'top_p' => (float)config('gpt.openai.top_p'),
+        //     'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
+        //     'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
+        //     'stop' => [config('gpt.openai.stop')],
+        // ])->json();
+
+// * ...
+
+        // $createdParaphraseId = Paraphrase::create([
+        //     'subject' => $request->subject,
+        //     'topic' => $request->topic,
+        //     'paraphrase' => $response['choices'][0]['message']['content'],
+        //     'user_id' => auth('sanctum')->user()->id,
+        // ])->id;
+
+        // $paraphrase = Paraphrase::with('user')->where('id', $createdParaphraseId)->first();
+
+// ? LITERATURE PARAPHRASE
+  // $response = Http::withHeaders([
+        //     'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
+        //     'Content-Type' => 'application/json',
+        // ])->post(config('gpt.openai.api_url'), [
+        //     'messages' => [
+        //         [
+        //             'role' => 'user',
+        //             'content' => 'Розкажи послідовно основні події літературного твору "' . $request->topic . '", дійові особи, тема, думка твору. ' . $paraphraseSizePrompt,
+        //         ]
+        //     ],
+
+        //     'model' => config('gpt.openai.model'),
+        //     'temperature' => (float)config('gpt.openai.temperature'),
+        //     'max_tokens' => config('gpt.openai.max_tokens'),
+        //     'top_p' => (float)config('gpt.openai.top_p'),
+        //     'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
+        //     'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
+        //     'stop' => [config('gpt.openai.stop')],
+        // ])->json();
+
+// ? TEST
+// $response = Http::withHeaders([
+        //     'Authorization' => 'Bearer ' . config('gpt.openai.api_key'),
+        //     'Content-Type' => 'application/json',
+        // ])->post(config('gpt.openai.api_url'), [
+        //     'response_format' => ['type' => 'json_object'],
+        //     'messages' => [
+        //         [
+        //             'role' => 'system',
+        //             'content' => 'Provide a valid JSON output'
+        //         ],
+        //         [
+        //             'role' => 'user',
+        //             'content' => 'Сфорулюй 5 тестових запитань до цього тексту ' . $paraphrase->paraphrase . ' , який є стислим переказом теми ' . $paraphrase->topic . ' з предмету ' . $paraphrase->subject . '. На кожне запитання 4 варіанти відповіді, де тільки одна правильна. Запитання мають бути виключно українською мовою. Всі запитання надай у полі "questions" як масив запитань. В об\'єкті питання має бути поле "question" значенням якого є текст запитання. Масив "options" з переліком варіантів відповіді. Та "correct" значення якого індекс правильного варіанту відповіді з масиву "options"',
+        //         ]
+        //     ],
+
+        //     'model' => config('gpt.openai.model'),
+        //     'temperature' => (float)config('gpt.openai.temperature'),
+        //     'max_tokens' => config('gpt.openai.max_tokens'),
+        //     'top_p' => (float)config('gpt.openai.top_p'),
+        //     'frequency_penalty' => (float)config('gpt.openai.frequency_penalty'),
+        //     'presence_penalty' => (float)config('gpt.openai.presence_penalty'),
+        //     'stop' => [config('gpt.openai.stop')],
+        // ])->json();
+
+        // $test = json_decode($response['choices'][0]['message']['content']);
+
+           // foreach ($test->questions as $question) {
+        //     $questionId = Question::create([
+        //         'question' => $question->question,
+        //         'paraphrase_id' => $paraphraseId,
+        //     ])->id;
+
+        //     for ($i = 0; $i < 4; $i++) {
+        //         Option::create([
+        //             'option' => $question->options[$i],
+        //             'correct' => $i == $question->correct,
+        //             'question_id' => $questionId,
+        //         ]);
+        //     }
+        // }
+
+        // $paraphrase = Paraphrase::where('id', $paraphraseId)->with('questions.options')->first();
